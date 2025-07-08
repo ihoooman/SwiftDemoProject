74a318d0cb3db6710554297fdd935c3f4c7e8611
Hooman Maddah <hoomi.hooman@gmail.com>
2025-07-08 15:16:23 +0330

    add icons

---
+++  generate-icons.sh
@@ -0,0 +1,25 @@
+#
+//   generate-icons.sh
+//  test
+//
+//  Created by Hooman Maddah on 7/8/25.
+//
+
+#!/usr/bin/env bash
+# آدرس پوشه‌ی AppIcon.appiconset در پروژه
+ICONSET="Assets.xcassets/AppIcon.appiconset"
+# نام فایل مبنا
+BASE_ICON="$ICONSET/icon.png"
+
+# اندازه‌های موردنیاز (pt) برای macOS
+sizes=(16 32 128 256 512)
+
+for pt in "${sizes[@]}"; do
+  # یک‌بار @1x
+  sips -Z $pt $BASE_ICON --out "$ICONSET/icon_${pt}x${pt}.png"
+  # یک‌بار @2x
+  scale2=$((pt * 2))
+  sips -Z $scale2 $BASE_ICON --out "$ICONSET/icon_${pt}x${pt}@2x.png"
+done
+
+echo "✅ Icons generated in $ICONSET"
