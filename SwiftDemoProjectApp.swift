bf89532a04d4c4c6e85658200fa952ea0ec24876
Hooman Maddah <hoomi.hooman@gmail.com>
2025-07-08 13:44:15 +0330

    Add App entry point, reformat ContentView, fix UI texts

---
+++ SwiftDemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled.swift
+//  test
+//
+//  Created by Hooman Maddah on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
